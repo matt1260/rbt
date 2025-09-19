5938d007e0d02e2659493d0c44af33d8b054a51f
Matt <mattp1260@gmail.com>
2025-09-19 18:33:20 +0200

    settings

--- hebrewtool/settings.py
+++ hebrewtool/settings.py
@@ -29,8 +29,7 @@ DEFAULT_CHARSET = 'utf-8'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = ["*"]
 
 CSRF_COOKIE_SECURE = True
