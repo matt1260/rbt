fff8815cf4d8e1b46dd05eabda883cc588a38b94
Matt <mattp1260@gmail.com>
2026-01-17 23:23:13 +0100

    CORS fix

--- hebrewtool/settings.py
+++ hebrewtool/settings.py
@@ -50,11 +50,32 @@ CSRF_TRUSTED_ORIGINS = ['https://mzg2o4p8.up.railway.app', 'https://rbtproject.u
 CORS_ALLOWED_ORIGINS = [
     'https://www.realbible.tech',
     'https://realbible.tech',
+    'https://read.realbible.tech',
     'http://127.0.0.1:8000',
     'http://localhost:8000',
-    'null',  # For file:// protocol testing
 ]
 CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
 
 LOGIN_URL = 'accounts/login/'
 
@@ -84,9 +105,9 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
